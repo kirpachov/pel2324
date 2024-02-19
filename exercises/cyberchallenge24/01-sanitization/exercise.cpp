b8b0797a7486e39dc5e37f74d587ea1a42f55380
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-02-19 15:44:02 +0100

    NEW lezione 19 feb 24

---
+++ exercises/cyberchallenge24/01-sanitization/exercise.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(void){
+	std::out << "Ciao";
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 
+// Esercizio 19 feb 24
+// Leggere una sequenza di int che termina quando vengono inseriti 2 numeri uguali consecutivi
+void es19feb24_1(void){
+	int lastone;
+	std::cin >> lastone;
+	bool found = false;
+	do {
+		int current;
+		std::cin >> current;
+		if (current == lastone) found = true;
+		else lastone = current;
+	} while(!found);
+
+	std::cout << "Trovato due volte il numero " << lastone << std::endl;
+}
+
+
+// Leggi 10 interi e stampa "Si" se almeno un elemento della sequenza è pari.
+// PATTER ESISTENZIALE
+void pattern_esistenziale(void){
+	bool found = false;
+	for(int i = 0; i < 10; i++){
+		int k;
+		std::cin >> k;
+		if (k % 2 == 0) found = true;
+	}
+
+	if (found) std::cout << "Si" << std::endl;
+}
+
+// Leggi 10 numeri da tastiera e stampa si se sono tutti maggiori di 100
+void pattern_universale(void){
+	bool risp = true;
+	for(int i = 0; i < 10; i++){
+		int k;
+		std::cin >> k;
+		if (k <= 100) risp = false;
+	}
+
+	if (risp) std::cout << "si" << std::endl;
+}
+
 int main() {
-  std::cout << "Hola!";
+
+	// es19feb24_1();
+	// pattern_esistenziale();
+	pattern_universale();
+
+	//for(int i = 0; i < 10; i++){
+	//	if (i % 2 == 0) continue;
+
+      	//	std::cout << "n " << i << std::endl;
+	//}
 
   return 0;
 }
