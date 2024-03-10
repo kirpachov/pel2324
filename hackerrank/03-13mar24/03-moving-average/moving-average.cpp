ba253f5e51e0e827a5deef7980da4d3091cd0785
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-03-10 13:20:28 +0100

    NEW third exercise of third exercitation on hackerrank

--- hackerrank/03-13mar24/03-moving-average/moving-average.cpp
+++ hackerrank/03-13mar24/03-moving-average/moving-average.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<iostream>
+#include<cassert>
 
 void print(const std::vector<double>& v) {
   for (auto x: v) {
@@ -20,14 +21,34 @@ void read(std::vector<double>& v) {
   }
 }
 
+double average(std::vector<double> const&v, const int orig_from, int to){
+  assert(orig_from <= to);
+  assert(to >= 0);
+
+  // In case invalid indexes are provided
+  int from = orig_from >= 0 ? orig_from : 0;
+  if (to > v.size() - 1) to = v.size() - 1;
+ 
+  double div = to - from + 1, acc = 0;
+
+  while(from <= to){
+    acc += v.at(from);
+    from++;
+  }
+
+  return acc / div;
+}
 
 std::vector<double> moving_average(const std::vector<double> v, int k) {
     std::vector<double> result(v.size());
 
-    for(auto a: v) std::cout << a<< std::endl;
+    for(int i = 0; i < v.size(); i++){
+      result.at(i) = average(v, i - k, i + k);
+    }
 
     return result;
 }
+
 int main() {
   std::vector<double> v;
 
